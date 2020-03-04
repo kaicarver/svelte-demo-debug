c5c76daec6085e9eb8d31171f898cf57e7e2b520
Kai Carver <kaicarver@gmail.com>
2020-03-04 18:32:07 +0100

    initial blank board, with appearance issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,48 @@
 <script>
-	export let name;
+  import Board from "./Board.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    font: 14px "Century Gothic", Futura, sans-serif;
+    margin: 20px;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  ol,
+  ul {
+    padding-left: 30px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .game {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .game-info {
+    margin-left: 20px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <div class="game">
+    <div class="game-board">
+      <Board />
+    </div>
+    <div class="game-info">
+      <div>
+        <!-- /* status */ -->
+      </div>
+      <ol>
+        <ul>
+          <!-- /* TODO */ -->
+        </ul>
+      </ol>
+    </div>
+  </div>
+</main>
---
+++ src/Board.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Square from "./Square.svelte";
+
+  let status = "Next player: X";
+</script>
+
+<style>
+  .board-row:after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+
+  .status {
+    margin-bottom: 10px;
+  }
+</style>
+
+<div>
+  <div class="status">{status}</div>
+  <div class="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  <div class="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  <div class="board-row">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+</div>

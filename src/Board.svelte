b1f27a17cf678971ff877e23f26faaea601a9a19
Kai Carver <kaicarver@gmail.com>
2020-03-13 23:50:52 +0100

    calculate winner

--- src/Board.svelte
+++ src/Board.svelte
@@ -4,6 +4,26 @@
   let status = "Next player: ";
   let squares = Array(9).fill(null);
   let onclick = 'X';
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 </script>
 
 <style>
@@ -19,7 +39,7 @@
 </style>
 
 <div>
-  <div class="status">{status} {onclick}</div>
+  <div class="status" on:click={alert(calculateWinner(squares))}>{status} {onclick}</div>
   <div class="board-row">
     <!-- I wonder if there's a way to number the squares by the DOM element number?... -->
     <Square bind:value={squares[0]} bind:onclick/>

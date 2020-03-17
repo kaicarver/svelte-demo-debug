d39ad3d7c16480267c28d11cd97bfa23f5709205
Kai Carver <kaicarver@gmail.com>
2020-03-18 00:13:27 +0100

    move status and brains to App/Game level

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,41 @@
   import Board from "./Board.svelte";
 
   let squares = Array(9).fill(null);
+  let nextValue = 'X';
+  let winner = null;
+  let status;
+
+  $: squares, console.log('squares has been updated, non-nulls: ' + squares.filter(x => x != null).length);
+  $: {
+    winner = calculateWinner(squares);
+    if (winner) {
+      status = "Winner: " + winner;
+      nextValue = null;
+    } else {
+      status = "Next player: " + nextValue;
+    }
+    // console.log("calculated") // why is this code executed twice for every click?
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 </script>
 
 <style>
@@ -27,6 +62,10 @@
   .game-info {
     margin-left: 20px;
   }
+  
+  .status {
+    margin-bottom: 10px;
+  }
 
   @media (min-width: 640px) {
     main {
@@ -36,13 +75,13 @@
 </style>
 
 <svelte:head>
-	<title>Svelte Tic-tac-toe</title>
+	<title>Svelte Tic-tac-toexx</title>
 </svelte:head>
 
 <main>
   <div class="game">
     <div class="game-board">
-      <Board bind:squares/>
+      <Board bind:squares bind:nextValue/>
       <p class="credits">
         <a href="https://github.com/kaicarver/svelte-demo">Source on Github</a>,
         adapted for
@@ -55,7 +94,7 @@
     </div>
     <div class="game-info">
       <div>
-        <!-- /* status */ -->
+        {status}
       </div>
       <ol>
         <ul>
--- src/Board.svelte
+++ src/Board.svelte
@@ -1,41 +1,8 @@
 <script>
   import Square from "./Square.svelte";
 
-  let status;
   export let squares;
-  let nextValue = 'X';
-  let winner = null;
-  $: squares, console.log('squares has been updated, non-nulls: ' + squares.filter(x => x != null).length);
-  $: {
-    winner = calculateWinner(squares);
-    if (winner) {
-      status = "Winner: " + winner;
-      nextValue = null;
-    } else {
-      status = "Next player: " + nextValue;
-    }
-    // console.log("calculated") // why is this code executed twice for every click?
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+  export let nextValue;
 </script>
 
 <style>
@@ -44,14 +11,9 @@
     content: "";
     display: table;
   }
-
-  .status {
-    margin-bottom: 10px;
-  }
 </style>
 
 <div>
-  <div class="status">{status}</div>
   <div class="board-row">
     <!-- I wonder if there's a way to number the squares by the DOM element number?... -->
     <Square bind:value={squares[0]} bind:nextValue/>
@@ -59,7 +21,6 @@
     <Square bind:value={squares[2]} bind:nextValue/>
   </div>
   <div class="board-row">
-  {@debug} 
     <Square bind:value={squares[3]} bind:nextValue/>
     <Square bind:value={squares[4]} bind:nextValue/>
     <Square bind:value={squares[5]} bind:nextValue/>

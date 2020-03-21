baf86197c38df5825989d739a310c3a98a45bd5b
Kai Carver <kaicarver@gmail.com>
2020-03-21 12:05:56 +0100

    more narrowing down

--- src/App.svelte
+++ src/App.svelte
@@ -15,37 +15,35 @@
   $: {
     history.push([...squares]);
     console.log(history)
-    winner = calculateWinner(squares);
-    if (winner) {
-      status = "Winner: " + winner;
-      nextValue = null;
-    } else {
-      status = "Next player: " + nextValue;
-    }
+    status = "Next player: " + nextValue;
     count = history.length;
     console.log("calculated") // why is this code executed twice for every click?
   }
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 </script>
+
+<svelte:head>
+	<title>Svelte DEBUG Tic-tac-toe</title>
+</svelte:head>
+
+<main>
+  <div class="game">
+    <div class="game-board">
+      <Board bind:squares bind:nextValue/>
+    </div>
+    <div class="game-info">
+      <div class="status">
+        {status}<br>
+        history size: {count}
+      </div>
+      <ol>
+        <ul>
+          <!-- /* TODO */ -->
+        </ul>
+      </ol>
+    </div>
+  </div>
+</main>
 
 <style>
   main {
@@ -82,34 +80,3 @@
   }
 </style>
 
-<svelte:head>
-	<title>Svelte Tic-tac-toexx</title>
-</svelte:head>
-
-<main>
-  <div class="game">
-    <div class="game-board">
-      <Board bind:squares bind:nextValue/>
-    </div>
-    <div class="game-info">
-      <div class="status">
-        {status}<br>
-        history size: {count}
-      </div>
-      <ol>
-        <ul>
-          <!-- /* TODO */ -->
-        </ul>
-      </ol>
-    </div>
-  </div>
-  <p class="credits">
-    <a href="https://github.com/kaicarver/svelte-demo">Source on Github</a>,
-    adapted for
-    <a href="https://svelte.dev/">Svelte</a>
-    after
-    <a href="https://github.com/kaicarver/react-demo">doing</a>
-    the excellent
-    <a href="https://reactjs.org/tutorial/tutorial.html">React tutorial</a>.
-  </p>
-</main>
